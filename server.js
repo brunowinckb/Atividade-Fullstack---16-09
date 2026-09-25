080d001e6eb48775baf4db5f626ddacf22001095
BRUNO WINCK BÚRIGO <bruno_w_burigo@estudante.sesisenai.org.br>
2026-09-25 12:47:33 -0300

    segundo commit

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from "express"
 
+const PORT = 3000
+
 const app = express() 
 
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000")
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
+
+app.get("/produtos", (req, res) => {
+    res.send("Lista de produtos")
 })
